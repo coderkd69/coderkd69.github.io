b88d9873ad9406763b8d7159a3f9e7206225920e
Ashish Ranjan <ashishranjan2912@gmail.com>
2017-09-08 19:31:49 +0530

    update hbd text color

--- js/hearts.js
+++ js/hearts.js
@@ -11,8 +11,6 @@ var love = setInterval(function() {
     var r_bg1 = Math.floor(Math.random() * 25) + 100;
     var r_time1 = Math.floor(Math.random() * 5) + 7;
 
-    //hide000WebhostFooter();
-    // var names = ["Mona", "Monti", "Chiku", "Asmi", "Asmita"]
     // var name = names[Math.floor(Math.random()*(names.length-1))]
     if(!checkTime()){
         $('.bg_heart').css("visibility", "hidden");
@@ -30,7 +28,7 @@ var love = setInterval(function() {
         var hbdTexts = ["Happyyy b'dayyy", "Shubho Jonmoh din!", "Happyyy b'dayyy"]
         var hbdText = hbdTexts[Math.floor(Math.random()*(hbdTexts.length-1))]
 
-        $('.bg_heart').append("<div class='heart-text' style='width: 90px;height: 68px;left:" + r_left1 + "%;-webkit-animation:love " + r_time1 + "s ease;-moz-animation:love " + r_time1 + "s ease;-ms-animation:love " + r_time1 + "s ease;animation:love " + r_time1 + "s ease; text-align: center; color: #ffffff'><div class='hbd-text'><p style=\"z-index: 1000; position:absolute;\">" + hbdText +"</p></div></div>");
+        $('.bg_heart').append("<div class='heart-text' style='width: 90px;height: 68px;left:" + r_left1 + "%;-webkit-animation:love " + r_time1 + "s ease;-moz-animation:love " + r_time1 + "s ease;-ms-animation:love " + r_time1 + "s ease;animation:love " + r_time1 + "s ease; text-align: center; color: #ffffff'><div class='hbd-text'><p style=\"z-index: 1000; position:absolute; color: #00de00;\">" + hbdText +"</p></div></div>");
 
 
         // $('.bg_heart').append("<div class='heart-text' style='width: 28px;height: 28px;left:" + (r_left1 + r_num1) + "%;-webkit-animation:love " + (r_time1 + 5) + "s ease;-moz-animation:love " + (r_time1 + 5) + "s ease;-ms-animation:love " + (r_time1 + 5) + "s ease;animation:love " + (r_time1 + 5) + "s ease; text-align: center; color: #ffffff'><div class='hbd-text'><p style=\"z-index: 1000; position:absolute;\">"+name+"</p></div></div>");
