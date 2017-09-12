b7caf870c21c1a05f4511cccc67b45d51839fb1d
Ashish Ranjan <ashishranjan2912@gmail.com>
2017-09-12 15:48:44 +0530

    implement dynamic bday date

--- js/timer.js
+++ js/timer.js
@@ -1,4 +1,16 @@
-var end = new Date('09/09/2017 12:00 AM');
+
+function getNextBdayDate(){
+    var date = '09/09/';
+    var current = new Date();
+    if(current.getMonth() < 8 || (current.getMonth() == 8 && current.getDate() <= 9)){
+        date += current.getFullYear();
+    }else{
+        date += (current.getFullYear()+1);
+    }
+    return date + ' 12:00 AM';
+}
+
+var end = new Date(getNextBdayDate());
 var animEndTime = new Date(end.getTime() + 60 * 60 * 24 * 1000);
 var _second = 1000;
 var _minute = _second * 60;
@@ -14,15 +26,15 @@ function showRemaining() {
         document.getElementById('countdown').innerHTML = 'NEXT YEAR MAYBE?';
         return;
     }
-    // var days = Math.floor(distance / _day);
+    var days = Math.floor(distance / _day);
     var hours = Math.floor((distance % _day) / _hour);
     var minutes = Math.floor((distance % _hour) / _minute);
     var seconds = Math.floor((distance % _minute) / _second);
 
-    // document.getElementById('countdown').innerHTML = days + 'days ';
-    document.getElementById('countdown').innerHTML = hours + ' hr' + (hours>1?'s':'') + ' ';
-    document.getElementById('countdown').innerHTML += minutes + ' min' + (minutes>1?'s':'') + ' ';
-    document.getElementById('countdown').innerHTML += seconds + ' sec' + (seconds>1?'s':'') + ' ';
+    document.getElementById('countdown').innerHTML = days + 'day' + (days>1?'s ':' ');
+    document.getElementById('countdown').innerHTML += hours + ' hr' + (hours>1?'s ':' ');
+    document.getElementById('countdown').innerHTML += minutes + ' min' + (minutes>1?'s ':' ');
+    document.getElementById('countdown').innerHTML += seconds + ' sec' + (seconds>1?'s':'');
 }
 
 timer = setInterval(showRemaining, 1000);
